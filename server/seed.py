60885994907f4ac035f60e170ec769aae6f97d5c
DanielSudiOdeny <daniel.odeny@student.moringaschool.com>
2024-06-24 17:58:44 +0300

    Add migrations to application and add seed data to database

--- server/seed.py
+++ server/seed.py
@@ -1,125 +1,77 @@
+import random
+from app import db, app
+from models.transactions import Transactions
+from models.user import User
+from faker import Faker
 
-from app import db, app, Transactions
-from datetime import datetime
+fake = Faker()
 
+# Example seed data without dates to test auto-generation
 seed_data = [
     {
-        "id": 2,
-        "date": "2019-12-01",
         "description": "South by Southwest Quinoa Bowl at Fresh & Co",
         "category": "Food",
-        "amount": -10.55
+        "amount": -10.55,
+        "merchant": "Fresh & Co"
     },
     {
-        "id": 3,
-        "date": "2019-12-02",
-        "description": "South by Southwest Quinoa Bowl at Fresh & Co",
-        "category": "Food",
-        "amount": -10.55
-    },
-    {
-        "id": 4,
-        "date": "2019-12-04",
         "description": "Sunglasses, Urban Outfitters",
         "category": "Fashion",
-        "amount": -24.99
+        "amount": -24.99,
+        "merchant": "Urban Outfitters"
     },
     {
-        "id": 5,
-        "date": "2019-12-06",
         "description": "Venmo, Alice Pays you for Burrito",
         "category": "Food",
-        "amount": 8.75
+        "amount": 8.75,
+        "merchant": ""
     },
     {
-        "id": 6,
-        "date": "2019-12-06",
         "description": "Chipotle",
         "category": "Food",
-        "amount": -17.59
+        "amount": -17.59, 
+        "merchant": ""
     },
     {
-        "id": 7,
-        "date": "2019-12-07",
         "description": "Birthday Check from Grandma",
         "category": "Gift",
-        "amount": 50
-    },
-    {
-        "id": 8,
-        "date": "2019-12-09",
-        "description": "Lyft Ride",
-        "category": "Transportation",
-        "amount": -13.25
-    },
-    {
-        "id": 9,
-        "date": "2019-12-11",
-        "description": "Paycheck from Bob's Burgers",
-        "category": "Income",
-        "amount": 1000
-    },
-    {
-        "id": 10,
-        "date": "2019-12-16",
-        "description": "Tickets, Flatiron Multiplex Cinemas",
-        "category": "Entertainment",
-        "amount": -24
-    },
-    {
-        "id": 11,
-        "date": "2019-12-16",
-        "description": "MTA Vending Machine: MetroCard",
-        "category": "Transportation",
-        "amount": -116.39
-    },
-    {
-        "id": 12,
-        "date": "2019-12-17",
-        "description": "Venmo, Pay Roommate for Rent",
-        "category": "Housing",
-        "amount": -975
-    },
-    {
-        "id": 13,
-        "date": "2022-07-09",
-        "description": "Office lunch",
-        "category": "Food",
-        "amount": 2000
-    },
-    {
-        "id": 14,
-        "date": "2022-07-09",
-        "description": "Office lunch Wednesday",
-        "category": "Food",
-        "amount": 3000
-    },
-    {
-        "id": 15,
-        "date": "2023-08-10",
-        "description": "Movies",
-        "category": "Entertainment",
-        "amount": 3
-    }]
-   
-def _parse_date(date_str):
-    # Add your date parsing logic here
-    return datetime.strptime(date_str, "%Y-%m-%d")
+        "amount": 50,
+        "merchant": ""
+    }
+]
 
 def seed_transactions():
     with app.app_context():
-        for data in seed_data:
-            transaction = Transactions(
-                id=data["id"],
-                date=_parse_date(data["date"]),
-                description=data["description"],
-                category=data["category"],
-                amount=data["amount"]
-            )
-            db.session.add(transaction)
+
+         # Seed users
+        print("Generating Users...👥")
+        users = []
+        for _ in range(5):
+            user = User(username=fake.user_name(), email=fake.email(), password_hash=fake.password())
+            db.session.add(user)
+            users.append(user)
 
         db.session.commit()
+        print("Users generated :D")
+        # Seed transactions
+        print("Generating Transactions...💲")
+        for data in seed_data:
+            transaction = Transactions(
+                description=data["description"],
+                category=data["category"],
+                amount=data["amount"],
+                merchant=data["merchant"],
+                user_id=random.choice(users).id
+            )
+            db.session.add(transaction)
+        
 
+        # Generate additional transactions using Faker
+        db.session.commit()
+        print("Transactions generated :D")
+        
+       
+        print("Seeding done🚀")
 
 if __name__ == "__main__":
     seed_transactions()
